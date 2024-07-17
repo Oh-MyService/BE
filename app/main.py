9170230fd962c029bda3a0a6a58b43d2ec28a422
charlie3965 <charlie39653965@gmail.com>
2024-07-17 13:36:57 +0900

    ,

--- app/main.py
+++ app/main.py
@@ -46,7 +46,7 @@ AUTHORIZATION_URL = "https://accounts.google.com/o/oauth2/auth"
 TOKEN_URL = "https://oauth2.googleapis.com/token"
 USER_INFO_URL = "https://www.googleapis.com/oauth2/v1/userinfo"
 
-@app.get("/login")
+@app.get("/api/login")
 async def login():
     try:
         return RedirectResponse(
