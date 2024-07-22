25e2e8ea3816147a8d2a6b86f2ee7f9e126380d5
vita <the777the7@gmail.com>
2024-07-23 07:42:17 +0900

    back

--- app/main.py
+++ app/main.py
@@ -143,11 +143,7 @@ async def user_info(access_token: Optional[str] = Cookie(None)): #,db: Session =
     
     if not access_token:
         logging.error("No access token found in cookies")
-
-        user_id = payload.get("user_id")
-        user = db.query(User).filter(User.id == user_id).first()
-       # raise HTTPException(status_code=401, detail="Not authenticated")
-        raise HTTPException( content={"user_id": user.id})
+        raise HTTPException(status_code=401, detail="Not authenticated")
     
     if access_token.startswith("Bearer "):
         access_token = access_token[len("Bearer "):]
