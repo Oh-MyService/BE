a64549d36e0e6b5313fcd9377c320d610c6f7cfe
SYEONIH <syeonih@naver.com>
2024-07-26 19:38:48 +0900

    cors

--- app/main.py
+++ app/main.py
@@ -63,7 +63,7 @@ async def add_cors_headers(request, call_next):
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
     logging.debug(f"Response headers: {response.headers}")
-    return responsea
+    return response
 
 # Google OAuth 관련
 CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
