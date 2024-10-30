f975f741ceb5d0a7b778cc3e1972abbe72297865
charlie3965 <charlie39653965@gmail.com>
2024-10-30 16:40:16 +0900

    step 8로 변경

--- app/main.py
+++ app/main.py
@@ -237,7 +237,7 @@ def create_prompt(
             "background_color": "",#str(background_color) if background_color is not None else None,
             "mood": "",#mood, 
             "cfg_scale": float(cfg_scale) if cfg_scale is not None else None, 
-            "sampling_steps": 5,
+            "sampling_steps": 8,
             "seed": random.randint(0, 999999) if seed == -1 else int(seed)
         }
 
